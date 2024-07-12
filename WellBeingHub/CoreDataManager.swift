a886e751afe07aac3869d926893bcaf77845a2a9
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2024-07-13 01:11:54 +0600

    Create CoreDataManager Class

---
+++ WellBeingHub/CoreDataManager.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataManager.swift
+//  WellBeingHub
+//
+//  Created by Mahmudul Hasan on 13/7/24.
+//
+
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "WellBeingHub")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func saveContext() {
+    
+        let context = persistentContainer.viewContext
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let error = error as NSError
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
