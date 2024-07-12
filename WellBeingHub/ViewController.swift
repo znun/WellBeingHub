bf8b0d8aadb9286d10091f2bd9566bc53ccf0405
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2024-07-12 14:35:27 +0600

    Remove Storyboard and set viewcontroller

---
+++ WellBeingHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WellBeingHub
+//
+//  Created by Mahmudul Hasan on 12/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Well Being Hub"
+    }
+
+
+}
+
